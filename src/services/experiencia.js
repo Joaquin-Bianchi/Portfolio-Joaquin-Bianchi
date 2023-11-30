16d31abe4b1ba468e65711c3b71c1047d287df94
Joaquin <joaqobianchi13@gmail.com>
2023-11-30 12:08:11 -0300

    correcion img de  garden

--- src/services/experiencia.js
+++ src/services/experiencia.js
@@ -10,7 +10,7 @@ const experiencia = [
   {
     ProjectName: "Garden Wise (Cuidado de plantas)",
     Description:
-      "Descripción del Proyecto 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. tetur adipiscing elit.tetur adipiscing elit.",
+      "Descripción del Proyecto . Lorem ipsum dolor sit amet, consectetur adipiscing elit. tetur adipiscing elit.tetur adipiscing elit.",
     Imagen: "assets/garden.png",
     LinkRepo: "https://github.com/No-Country/S11-19-ft-php-next",
     LinkDemo: "https://s11-19-ft-php-next.vercel.app/",
