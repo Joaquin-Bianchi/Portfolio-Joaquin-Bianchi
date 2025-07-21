0bf43f8586f8511fa5822a0eff019bec6d04a6a6
Joaquin <joaqobianchi13@gmail.com>
2025-07-21 01:57:55 -0300

    Actuliazada experiencia y descripcion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,10 +35,15 @@ function About() {
           {/* text-wrap */}
           <div className="flex flex-col items-center mx-2 ml-5  sm:w-auto md:w-2/3 xl:w-6/12 sm:mx-0 relative">
             <p className="   text-wrap animate-fade-in animate-delay-500 text-base md:text-lg mt-1 lg:ml-5 text-left   lg:text-  dark:font-medium  text-black lg:text-left  font-Hind-Madurai  dark:text-[#cdcdcd] ">
-              Cuento con mas de 1 año de experiencia en el desarrollo y diseño
-              de aplicaciones web, Disfruto trabajando en equipo y también creo
-              que tengo buen gusto por el diseño. Actualmente estoy buscando un
-              equipo de IT donde pueda seguir creciendo profesionalmente.
+              Cuento con mas de 1 año experiencia en el diseño y desarrollo de
+              aplicaciones. Me apasiona crear productos que realmente aporten
+              valor, con interfaces claras, funcionales y pensadas para el
+              usuario. <br /> Disfruto colaborar en equipos donde se combina la
+              tecnología y el diseño, aportando desde lo visual y lo técnico
+              para lograr soluciones completas. Creo que el desarrollo de
+              productos es mucho más que código: es entender el problema,
+              trabajar en equipo y construir experiencias que realmente
+              funcionen.
             </p>
           </div>
           <div className="items-center mt-2  lg:flex animate-fade-in-up animate-delay-500 ">
